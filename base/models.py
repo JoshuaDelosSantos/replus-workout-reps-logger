1ebfe93aaa261691e5a1e98e1ed8e5a4d330b63b
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-10-16 20:48:35 +1000

    Add slug fields in Session and Exercise models

--- base/models.py
+++ base/models.py
@@ -13,29 +13,44 @@ NUMBER_DECIMAL_PLACE = 2
 
 class Session(models.Model):
     name = models.CharField(max_length=MAX_CHAR_FIELD, unique=True)  # Django will raise an IntegrityError when creating a duplicate.
+    slug = models.SlugField(unique=True, blank=True)
     
     def __str__(self):
         return self.name
     
     def save(self, *args, **kwargs):
         """
-        Override the save method to enforce uniqueness of the session name.
-        
-        If a session with the same name already exists in the database and 
-        this is a new instance (self.pk is None), raise a ValidationError.
-        This prevents duplicate session names from being saved.
+        Override the save method to enforce uniqueness of the session name and 
+        automatically set the slug based on the session name.
         """
+        # Check for duplicate session names
         if Session.objects.filter(name=self.name).exists() and not self.pk:
             raise ValidationError(f"A session with the name '{self.name}' already exists.")
+
+        # Automatically generate the slug from the name
+        if not self.slug or self.slug != slugify(self.name):
+            self.slug = slugify(self.name)
+        
         super().save(*args, **kwargs)
     
 
 class Exercise(models.Model):
     name = models.CharField(max_length=MAX_CHAR_FIELD)
     session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='exercises', null=True)
+    slug = models.SlugField(unique=True, blank=True)
     
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        """
+        Override the save method to automatically set the slug based on the exercise name.
+        """
+        # Automatically generate the slug from the name
+        if not self.slug or self.slug != slugify(self.name):
+            self.slug = slugify(self.name)
+        
+        super().save(*args, **kwargs)
     
     def determine_average_reps(self):
         total_reps = sum(line.reps for line in self.lines.all())
