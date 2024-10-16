9efc98acda0436060b27d1a176c3d8ac9a0e6e00
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-10-16 20:54:50 +1000

    Register models in admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Session, Exercise, Line
 
-# Register your models here.
+admin.site.register(Session)
+admin.site.register(Exercise)
+admin.site.register(Line)
