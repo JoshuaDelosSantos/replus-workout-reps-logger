c81a7c42c2353f2a591134f7ddb63d314c0bfb82
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-10-28 23:32:04 +1000

    Refactor to use class based view

--- users/urls.py
+++ users/urls.py
@@ -6,11 +6,12 @@ Date: 17/10/2024
 """
 
 from django.urls import path
-from .view.register_view import RegisterView 
-from . import views
+from .views.register_view import RegisterView 
+from .views.login_view import LoginView 
+from .views.logout_view import LogoutView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),  # Map the URL to RegisterView
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout')
+    path('register/', RegisterView.as_view(), name='register'),  
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
