c74d39ca9b32f8844f09cd7d2b61355d83179e32
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-10-29 21:35:28 +1000

    Add sessions path

--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,9 @@ Date: 02/09/2024
 from django.urls import path
 from . import views
 from .views.home_view import HomeView
+from .views.sessions_view import SessionsView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('sessions/', SessionsView.as_view(), name="sessions_view"),
 ]
