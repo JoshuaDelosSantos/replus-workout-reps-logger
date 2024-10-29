daba2a52dc9d3536b396f367b0b3c09164a6ce66
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-10-29 19:12:52 +1000

    Refactor code to remove duplicate function definition

--- base/models.py
+++ base/models.py
@@ -27,7 +27,6 @@ class Session(models.Model):
         Override the save method to enforce uniqueness of the session name and 
         automatically set the slug based on the session name.
         """
-    def save(self, *args, **kwargs):
         if Session.objects.filter(name=self.name, user=self.user).exists() and not self.pk:
             raise ValidationError(f"A session with the name '{self.name}' already exists.")
         
