f35a807ca9bb6ca739f2f65704b76bb2f594495e
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-10-30 23:39:13 +1000

    Add path to a specific exercise for a session

--- base/urls.py
+++ base/urls.py
@@ -9,8 +9,10 @@ from django.urls import path
 from . import views
 from .views.home_view import HomeView
 from .views.sessions_view import SessionsView
+from .views.exercise_view import ExerciseView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('sessions/', SessionsView.as_view(), name="sessions"),
+    path('sessions/<slug:session_slug>/', ExerciseView.as_view(), name='exercise'),
 ]
