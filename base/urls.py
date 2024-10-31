2af484d714bb80a1f116ab92cac31238c999052f
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-10-31 17:57:37 +1000

    Add path for exercise lines view

--- base/urls.py
+++ base/urls.py
@@ -10,9 +10,11 @@ from . import views
 from .views.home_view import HomeView
 from .views.sessions_view import SessionsView
 from .views.exercise_view import ExerciseView
+from .views.lines_view import LinesView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('sessions/', SessionsView.as_view(), name="sessions"),
     path('sessions/<slug:session_slug>/', ExerciseView.as_view(), name='exercise'),
+    path('sessions/<slug:session_slug>/<slug:exercise_slug>/', LinesView.as_view(), name='lines'),
 ]
